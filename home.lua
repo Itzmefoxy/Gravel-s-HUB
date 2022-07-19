5fce8bbb92032aa8322ae04fb74d0b758bdf428c
Itzmefoxy <68133216+Itzmefoxy@users.noreply.github.com>
2022-07-19 19:47:15 +0700

    homies

---
+++ home.lua
@@ -0,0 +1,10 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Gravel's Hub BUILD A BOAT", "Midnight")
+
+local Tab = Window:NewTab("Home")
+
+local HomeSection = Tab:NewSection("Welcome to Gravel's hub")
+
+HomeSection:NewButton("Build a boat", "Show Build a boat version", function()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Itzmefoxy/Gravel-s-HUB/main/build%20a%20boat.lua")))(
+end)
