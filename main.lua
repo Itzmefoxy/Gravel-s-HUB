262ea65cb75fcf0873c0029ff8eeadfbaaba66be
Itzmefoxy <68133216+Itzmefoxy@users.noreply.github.com>
2022-07-19 19:43:33 +0700

    shut up

---
+++ main.lua
@@ -0,0 +1,16 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Gravel's Hub BUILD A BOAT", "Midnight")
+
+local Tab = Window:NewTab("Home")
+
+local HomeSection = Tab:NewSection("Welcome to Gravel's hub")
+
+HomeSection:NewLabel("Please enter the key from discord")
+
+HomeSection:NewTextBox("TextboxText", "TextboxInfo", function(txt)
+	if txt == "QelNPc1sCueI6jlyVtvg045G2VraBRQPTcNRYNVii9H1bNfupz5eTE4WhMBI" then
+      loadstring()
+      else
+      print("Key Invalid!")
+      end
+end)
