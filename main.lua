8f7ccdbd2576248cb430c3b0747e4fe2a0d6e588
Itzmefoxy <68133216+Itzmefoxy@users.noreply.github.com>
2022-07-20 16:04:40 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,25 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Gravel's Hub ENTER KEY", "Midnight")
+local Window = Library.CreateLib("Gravel's Hub ENTER KEY", "Ocean")
 
 local Tab = Window:NewTab("Home")
 
 local HomeSection = Tab:NewSection("Welcome to Gravel's hub")
 
-HomeSection:NewLabel("Please enter the key from discord")
-
-HomeSection:NewTextBox("TextboxText", "TextboxInfo", function(txt)
+HomeSection:NewTextBox("Key", "TextboxInfo", function(txt)
 	if txt == "QelNPc1sCueI6jlyVtvg045G2VraBRQPTcNRYNVii9H1bNfupz5eTE4WhMBI" then
-      loadstring(game:HttpGet(("https://raw.githubusercontent.com/Itzmefoxy/Gravel-s-HUB/main/home.lua")))(
+      loadstring(game:HttpGet(("https://raw.githubusercontent.com/Itzmefoxy/Gravel-s-HUB/main/home.lua")))()
+      elseif txt == "im a dumbass" then 
+      game:GetService("StarterGui"):SetCore("SendNotification", { 
+        Title = "Ok.";
+        Text = "I'll give you the script after you enter the key from the discord";
+        Icon = "rbxthumb://type=Asset&id=7203162490&w=150&h=150"})
+    Duration = 16;
       else
       print("Key Invalid!")
       end
+      
+
+
 end)
+
+HomeSection:NewLabel("Please enter the key from the discord")
